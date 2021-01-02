0d5f063c6ec68bce31651b469ad97565544882e6
haile1999 <haile3107@gmail.com>
2021-01-02 09:01:48 +0700

    game play

--- src/frontend/user/src/components/Game/Game.js
+++ src/frontend/user/src/components/Game/Game.js
@@ -55,6 +55,162 @@ const Game=(props)=>{
             </header>
         </div>
     )
+    function checkWin(row, col, user, stepNumber) {
+
+        if (stepNumber === 0) {
+            return null;
+        }
+
+        const { history } = props;
+        const current = history[stepNumber];
+        const squares = current.squares.slice();
+
+        // Get coordinates
+        let coorX = row;
+        let coorY = col;
+ 
+        let countCol = 1;
+        let countRow = 1;
+        let countMainDiagonal = 1;
+        let countSkewDiagonal = 1;
+        let isBlock;
+        const rival = (user === Config.xPlayer) ? Config.oPlayer : Config.xPlayer;
+ 
+        // Check col
+        isBlock = true;
+        let winCells = [];
+        coorX -= 1;
+        while(coorX >= 0 && squares[coorX][coorY] === user) {
+            countCol += 1;
+            winCells.push([coorX, coorY]);
+            coorX -= 1;
+        }
+        if (coorX >= 0 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorX = row;
+        winCells.push([coorX, coorY]);
+        coorX += 1;
+        while(coorX <= Config.brdSize - 1 && squares[coorX][coorY] === user) {
+            countCol += 1;
+            winCells.push([coorX, coorY]);
+            coorX += 1;
+        }
+        if (coorX <= Config.brdSize - 1 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorX = row;
+        if (isBlock === false && countCol >= 5) return winCells;
+ 
+        // Check row
+        isBlock = true;
+        winCells = [];
+        coorY -= 1;
+        while(coorY >= 0 && squares[coorX][coorY] === user) {
+            countRow += 1;
+            winCells.push([coorX, coorY]);
+            coorY -= 1;
+        }
+        if (coorY >= 0 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorY = col;
+        winCells.push([coorX, coorY]);
+        coorY += 1;
+        while(coorY <= Config.brdSize - 1 && squares[coorX][coorY] === user) {
+            countRow += 1;
+            winCells.push([coorX, coorY]);
+            coorY += 1;
+        }
+        if (coorY <= Config.brdSize - 1 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorY = col;
+        if (isBlock === false && countRow >= 5) return winCells;
+
+        // Check main diagonal
+        isBlock = true;
+        winCells = [];
+        coorX -= 1;
+        coorY -= 1;
+        while(coorX >= 0 && coorY >= 0 && squares[coorX][coorY] === user) {
+            countMainDiagonal += 1;
+            winCells.push([coorX, coorY]);
+            coorX -= 1;
+            coorY -= 1;
+        }
+        if (coorX >= 0 && coorY >= 0 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorX = row;
+        coorY = col;
+        winCells.push([coorX, coorY]);
+        coorX += 1;
+        coorY += 1;
+        while(coorX <= Config.brdSize - 1 && coorY <= Config.brdSize - 1 && squares[coorX][coorY] === user) {
+            countMainDiagonal += 1;
+            winCells.push([coorX, coorY]);
+            coorX += 1;
+            coorY += 1;
+        }
+        if (coorX <= Config.brdSize - 1 && coorY <= Config.brdSize - 1 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorX = row;
+        coorY = col;
+        if (isBlock === false && countMainDiagonal >= 5) return winCells;
+
+        // Check skew diagonal
+        isBlock = true;
+        winCells = [];
+        coorX -= 1;
+        coorY += 1;
+        while(coorX >= 0 && coorY >= 0 && squares[coorX][coorY] === user) {
+            countSkewDiagonal += 1;
+            winCells.push([coorX, coorY]);
+            coorX -= 1;
+            coorY += 1;
+        }
+        if (coorX >= 0 && coorY >= 0 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        coorX = row;
+        coorY = col;
+        winCells.push([coorX, coorY]);
+        coorX += 1;
+        coorY -= 1;
+        while(coorX <= Config.brdSize - 1 && coorY <= Config.brdSize - 1 && squares[coorX][coorY] === user) {
+            countSkewDiagonal += 1;
+            winCells.push([coorX, coorY]);
+            coorX += 1;
+            coorY -= 1;
+        }
+        if (coorX <= Config.brdSize - 1 && coorY <= Config.brdSize - 1 && squares[coorX][coorY] !== rival) {
+            isBlock = false;
+        }
+        if (isBlock === false && countSkewDiagonal >= 5) return winCells;
+
+        return null;
+    }
+
+    function userClick(row, col) {
+        const { nextMove } = props;
+
+        // Prevent user click if rival is disconnected
+        if (needToDisable) {
+            return;
+        }
+
+        // Prevent user click if not his turn
+        if ((isPlayerX && nextMove === Config.oPlayer) || (!isPlayerX && nextMove === Config.xPlayer)) {
+            return;
+        }
+        
+        // Send move to server if it is valid
+        if (handleClick(row, col)) {
+            socket.emit('move', { row: row, col: col });
+        }
+    }
 }
 
 export default Game;
