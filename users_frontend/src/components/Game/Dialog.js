3b990e42eed40c783c66bd642c7ed13909020f77
danh phi long <long9ka@gmail.com>
2021-01-13 12:01:59 +0700

    fix user.cup

--- users_frontend/src/components/Game/Dialog.js
+++ users_frontend/src/components/Game/Dialog.js
@@ -1,4 +1,4 @@
-import React,{useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
@@ -11,26 +11,29 @@ function WinDialog(props) {
   const { winner, open, cup, room, socket } = props;
   const history = useHistory();
   const [user, setUser] = useContext(UserCtx);
-  const handlePlayAgain =()=>{
+  const handlePlayAgain = () => {
 
   };
-  const handleOut = ()=>{
-      nspRooms.emit("leave_room", room, user);
-      history.push('/');
+  const handleOut = () => {
+    nspRooms.emit("leave_room", room, user);
+    history.push('/');
   };
-  return (
-    <Dialog className="dialog" aria-labelledby="simple-dialog-title" open={open}>
+
+  if (user) {
+    return (
+      <Dialog className="dialog" aria-labelledby="simple-dialog-title" open={open}>
         <DialogTitle id="simple-dialog-title">
-            {winner === "you" ?
-             `Chúc mừng! Bạn đã chiến thắng. \n Bạn nhận được ${cup} cup`
-                
-              : `Bạn đã thua! \n Bạn bị trừ ${cup} cup`}
+          {winner === "you" ?
+            `Chúc mừng! Bạn đã chiến thắng. \n Bạn nhận được ${cup} cup`
+
+            : `Bạn đã thua! \n Bạn bị trừ ${cup} cup`}
         </DialogTitle>
-        <p>Số cúp: {winner==="you" ? (user.cup + cup) : (user.cup - cup)}</p>
+        <p>Số cúp: {winner === "you" ? (user.cup + cup) : (user.cup - cup)}</p>
         <Button variant="contained" color="primary" onClick={handlePlayAgain}>Choi Lai</Button>
         <Button variant="contained" color="secondary" onClick={handleOut}>Thoat</Button>
-    </Dialog>
-  );
+      </Dialog>
+    );
+  } else return null;
 }
 
 export default WinDialog;
